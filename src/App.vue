9ca204468dc352360f73d34aebc50fd09470d168
TawhidSajib <tauhidulsajib34@gmail.com>
2021-05-20 13:18:07 +0600

    navbar updated

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,60 @@
 <template>
   <div id="app">
+    <nav class="flex items-center justify-between flex-wrap bg-blue-500 p-6">
+      <div class="flex items-center flex-shrink-0 text-white mr-6">
+        <span class="font-semibold text-xl tracking-tight">Blog</span>
+      </div>
+      <div class="block lg:hidden">
+        <button
+          class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
+        >
+          <svg
+            class="fill-current h-3 w-3"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>Menu</title>
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+          </svg>
+        </button>
+      </div>
+      <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+        <div class="text-sm lg:flex-grow">
+          <router-link
+            tag="a"
+            to="/"
+            href="#responsive-header"
+            class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+          >
+            Home
+          </router-link>
+          <router-link
+            tag="a"
+            to="/users"
+            href="#responsive-header"
+            class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+          >
+            Users
+          </router-link>
+          <router-link
+            tag="a"
+            to="/blog"
+            href="#responsive-header"
+            class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+          >
+            Blog List
+          </router-link>
+          <router-link
+            tag="a"
+            to="/gallery"
+            href="#responsive-header"
+            class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
+          >
+            Gallery
+          </router-link>
+        </div>
+      </div>
+    </nav>
     <router-view></router-view>
   </div>
 </template>
